d64197d5bc988492934a70fcc78f6bd60d2fbaf8
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-15 19:41:15 +0600

    contact

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ var btns = header.getElementsByClassName("btn");
 const active = (hash) => {
    for (let i = 0; i < isotopContent.length; i++) {
       const element = isotopContent[i];
-      if (element.id.split(" ").includes(hash.slice(1))) {
+      if (element.id.split(" ").includes(hash)) {
          element.classList.add("active-isotop");
          element.classList.remove("inactive-isotop");
-      } else if (hash === "#all") {
+      } else if (hash === "all") {
          element.classList.add("active-isotop");
          element.classList.remove("inactive-isotop");
       } else {
@@ -20,9 +20,30 @@ const active = (hash) => {
 
 for (var i = 0; i < btns.length; i++) {
    btns[i].addEventListener("click", function (e) {
-      active(e.target.hash);
+      active(e.target.id);
       var current = document.getElementsByClassName("active-btn");
       current[0].className = current[0].className.replace(" active-btn", "");
       this.className += " active-btn";
    });
 }
+
+function init_map() {
+   var myOptions = {
+      zoom: 14,
+      center: new google.maps.LatLng(23.8103, 90.4125),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+   };
+   map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+   marker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(23.8103, 90.4125),
+   });
+   infowindow = new google.maps.InfoWindow({
+      content: "<strong>My Location</strong><br>Dhaka<br>",
+   });
+   google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(map, marker);
+   });
+   infowindow.open(map, marker);
+}
+google.maps.event.addDomListener(window, "load", init_map);
